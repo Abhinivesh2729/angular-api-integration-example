162f1027bd30aee5f055f04da3449d06824facf3
Abhinivesh2729 <abhinivesh@utsahatechnologies.com>
2025-08-23 09:33:27 +0530

    updated products

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App  implements OnInit{
   protected readonly title = signal('products');
+  products: any[] = []
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadProducts()
+  }
+
+  loadProducts() {
+    this.http.get('https://api.escuelajs.co/api/v1/products').subscribe({
+      next: (data: any) => {
+        this.products = data
+        console.log(data)
+      }
+    })
+  }
+
 }
